59f50feea2aba85caa977a5cffa6befe602a1fd6
ArthurEstevaum <arthurestevao631@gmail.com>
2025-10-03 22:07:50 -0300

    Bootstrap spring boot

---
+++ src/test/java/com/coda_fofos/java_akademika/JavaAkademikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coda_fofos.java_akademika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAkademikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
